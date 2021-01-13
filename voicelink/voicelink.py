61df33b4e6bdc5fbbd591eac89c692b129ac2eec
ManGoYTB <mangoytbfr@gmail.com>
2021-01-13 23:34:58 +0100

    Add files via upload

---
+++ voicelink/voicelink.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+class VoiceLink(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, vcid: int):
+        if ctx.author.id == 765635946539384882:
+            if ctx.voice_client is not None:
+                await ctx.voice_client.disconnect()
+                vc = self.bot.get_channel(vcid)
+                await vc.connect()
+                await ctx.send("Done")
+            else:
+                vc = self.bot.get_channel(vcid)
+                await vc.connect()
+                await ctx.send("Done")
+        else:
+            return
+
+    @commands.command()
+    async def leave(self, ctx, vcid: int):
+        if ctx.author.id == 765635946539384882:
+            #vc = self.bot.get_channel(vcid)
+            await ctx.voice_client.disconnect()
+            await ctx.send("Done")
+        else:
+            return
+
+def setup(bot):
+    bot.add_cog(VoiceLink(bot))
